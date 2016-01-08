55382382d6026aef82ce9dec6d0d23ff1275431c
TomyAngelo <tomyangelo1@azet.sk>
2016-01-08 10:27:52 +0100

    essiv and xts do not work

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <openssl/aes.h>
 #include <openssl/sha.h>
+#include <openssl/modes.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -11,6 +12,13 @@ void increaseVector(unsigned char * initialVector, int sector){
     for(int i = BLOCK_SIZE-1; i != 0 ; --i){
           initialVector[i]=sector & 0xFF;
           sector=sector>>8;
+    }
+}
+
+void increaseVector2(unsigned char * initialVector, int sector){
+    for(int i = 0; i != BLOCK_SIZE-1 ; ++i){
+          initialVector[i]=sector & 0xFF;
+          sector=sector<<8;
     }
 }
 
@@ -55,6 +63,7 @@ int main(void)
     unsigned char *out_block_512 = (unsigned char*) malloc(SECTOR_SIZE);
 
     unsigned char initialVector[BLOCK_SIZE]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    //uint64_t initialVector=0;
 
     int sector=0;
 
@@ -63,12 +72,11 @@ int main(void)
         fwrite(out_block_512,SECTOR_SIZE,1,plaintext_cbc_plain64);
         memset(initialVector,0,BLOCK_SIZE);
         ++sector;
-        increaseVector(initialVector,sector);
+        increaseVector2(initialVector,sector);
     }
 
     fclose(plaintext_cbc_plain64);
-
-
+//---------------------------------------------------------------------------------
 
     FILE * ciphertext_cbc_essiv_sha256;
     FILE * plaintext_cbc_essiv_sha256;
@@ -76,25 +84,59 @@ int main(void)
     plaintext_cbc_essiv_sha256=fopen("plaintext_aes-cbc-essiv_sha256.img","wb");
 
     memset(initialVector,0,BLOCK_SIZE);
-
-
     sector=0;
+
+    AES_KEY key_Dh;
+    unsigned char hash_key[BLOCK_SIZE];
+    SHA256_CTX sha_key;
+    SHA256_Init(&sha_key);
+    SHA256_Update(&sha_key, key, BLOCK_SIZE);
+    SHA256_Final(hash_key, &sha_key);
+
+    AES_set_decrypt_key(hash_key,256,&key_Dh);
+
+    unsigned char encryptedVector[BLOCK_SIZE];
+    unsigned char initialVectorCopy[BLOCK_SIZE];
+
     while (fread(block_512,SECTOR_SIZE,1,ciphertext_cbc_essiv_sha256) == 1){
 
-        unsigned char hash[BLOCK_SIZE];
-        SHA256_CTX sha256;
-        SHA256_Init(&sha256);
-        SHA256_Update(&sha256, initialVector, BLOCK_SIZE);
-        SHA256_Final(hash, &sha256);
+        memcpy(initialVectorCopy,initialVector,BLOCK_SIZE);
+        AES_cbc_encrypt(initialVector,encryptedVector,BLOCK_SIZE,&key_Dh,initialVectorCopy,AES_DECRYPT);
 
-        AES_cbc_encrypt(block_512,out_block_512,SECTOR_SIZE,&key_D,hash,AES_DECRYPT);
+        AES_cbc_encrypt(block_512,out_block_512,SECTOR_SIZE,&key_D,encryptedVector,AES_DECRYPT);
         fwrite(out_block_512,SECTOR_SIZE,1,plaintext_cbc_essiv_sha256);
+        memset(initialVector,0,BLOCK_SIZE);
+        ++sector;
+        increaseVector2(initialVector,sector);
+    }
+
+    fclose(plaintext_cbc_essiv_sha256);
+
+    //########################
+    //######### XTS ##########
+    //########################
+
+    FILE * ciphertext_xts;
+    FILE * plaintext_xts;
+    ciphertext_xts=fopen("aes-xts-plain64.img","rb");
+    plaintext_xts=fopen("plaintext-aes-xts.img","wb");
+
+    unsigned char *block_xts = (unsigned char*) malloc(BLOCK_SIZE);
+    unsigned char *out_block_xts = (unsigned char*) malloc(BLOCK_SIZE);
+
+    memset(initialVector,0,BLOCK_SIZE);
+    sector=0;
+    while (fread(block_xts,BLOCK_SIZE,1,ciphertext_xts) == 1){
+        XTS128_CONTEXT *ctx;
+
+
+        //CRYPTO_xts128_encrypt(ctx,initialVector,block_xts,out_block_xts,BLOCK_SIZE,AES_ENCRYPT);
+        fwrite(out_block_xts,BLOCK_SIZE,1,plaintext_xts);
         memset(initialVector,0,BLOCK_SIZE);
         ++sector;
         increaseVector(initialVector,sector);
     }
-    fclose(plaintext_cbc_essiv_sha256);
-
+    fclose(plaintext_xts);
 
     return 0;
 }
